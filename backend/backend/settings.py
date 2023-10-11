56b27cb80dc4fcc9454375d9cc29e554185af34e
GVU <vitalygagarin89@gmail.com>
2023-10-11 22:58:44 +1000

    backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['vitalygagarin.servebeer.com', '87.225.106.220', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['vitalygagarin.servebeer.com', 'www.vitalygagarin.servebeer.com']
 
 
 # Application definition
